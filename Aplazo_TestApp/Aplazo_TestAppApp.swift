ead3c967db94e9c51aecca3ce7fd3ea4c239a5b6
Daniel Meneses León <daniel.meneses@wgu.edu>
2022-01-06 16:53:54 -0600

    Initial Commit

---
+++ Aplazo_TestApp/Aplazo_TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aplazo_TestAppApp.swift
+//  Aplazo_TestApp
+//
+//  Created by Daniel Meneses León on 06/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Aplazo_TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aplazo_TestAppUITests/Aplazo_TestAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Aplazo_TestAppUITestsLaunchTests.swift
+//  Aplazo_TestAppUITests
+//
+//  Created by Daniel Meneses León on 06/01/22.
+//
+
+import XCTest
+
+class Aplazo_TestAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
